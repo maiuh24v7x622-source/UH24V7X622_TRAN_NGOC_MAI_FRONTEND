c1d447e8a0454e6906574445097140cc0e622f22
Tran Ngoc Mai <maiuh24v7x622maiuh24v7x622@dttx.ctu.edu.vn>
2026-03-15 07:33:27 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,12 @@ const routes = [
   },
 
   {
+    path: "/contacts/add",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+  },
+  
+  {
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -26,12 +26,12 @@
         <i class="fas fa-redo"></i> Làm mới
       </button>
 
-      <button
+      <router-link
+        :to="{ name: 'contact.add' }"
         class="btn btn-sm btn-success"
-        @click="goToAddContact"
       >
         <i class="fas fa-plus"></i> Thêm mới
-      </button>
+      </router-link>
 
       <button
         class="btn btn-sm btn-danger"
@@ -51,10 +51,9 @@
     <ContactCard :contact="activeContact" />
     <router-link
       :to="{ name: 'contact.edit', params: { id: activeContact._id } }"
+      class="btn btn-warning"
     >
-      <span class="mt-2 badge badge-warning">
-        <i class="fas fa-edit"></i> Hiệu chỉnh
-      </span>
+      <i class="fas fa-edit"></i> Hiệu chỉnh
     </router-link>
   </div>
 </div>
